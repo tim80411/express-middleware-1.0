567a675997563a8b10cf0140e1df6a3ac5440deb
tim80411 <tim80411@gmail.com>
2021-05-18 21:33:50 +0800

    feat: add req to res total time log

--- app.js
+++ app.js
@@ -4,23 +4,33 @@ const app = express()
 const port = 3000
 
 app.use(function (req, res, next) {
-  const timeStamp = Date.now()
+  const reqTimeStamp = Date.now()
 
-  const currentDate = new Date(timeStamp)
-  const year = currentDate.getFullYear()
-  const month = '0' + (currentDate.getMonth() + 1)
-  const date = '0' + currentDate.getDate()
-  const hours = currentDate.getHours()
-  const minutes = "0" + currentDate.getMinutes()
-  const seconds = "0" + currentDate.getSeconds()
+  function getFormatTime(timeStampNum) {
+    const currentDate = new Date(timeStampNum)
+    const year = currentDate.getFullYear()
+    const month = '0' + (currentDate.getMonth() + 1)
+    const date = '0' + currentDate.getDate()
+    const hours = currentDate.getHours()
+    const minutes = "0" + currentDate.getMinutes()
+    const seconds = "0" + currentDate.getSeconds()
 
-  const formattedTime = `${year}-${month.slice(-2)}-${date.slice(-2)} ${hours}:${minutes.slice(-2)}:${seconds.slice(-2)}`
+    const formattedTime = `${year}-${month.slice(-2)}-${date.slice(-2)} ${hours}:${minutes.slice(-2)}:${seconds.slice(-2)}`
 
-  console.log(`${formattedTime} | ${req.method} from ${req.originalUrl}`)
+    return formattedTime
+  }
+  console.log(`${getFormatTime(reqTimeStamp)} | ${req.method} from ${req.originalUrl}`)
+
+  res.on('close', () => {
+    const resTimeStamp = Date.now()
+    const timePass = resTimeStamp - reqTimeStamp
+
+    console.log(`${getFormatTime(resTimeStamp)} | ${req.method} from ${req.originalUrl} | total time: ${timePass} ms`)
+  })
   next()
 })
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.send(`
     <a href="/new">GET /new</a><br/>
     <a href="/something">GET /something</a><br/>
@@ -29,6 +39,7 @@ app.get('/', (req, res) => {
     </form>
     `
   )
+  next()
 })
 
 app.get('/new', (req, res) => {
